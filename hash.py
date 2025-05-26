4a8fef3e643fd8fbdbec930c148abc2e026a9925
Abdulmajeed Nomman <m3nt@hotmail.com>
2025-05-27 02:02:24 +0300

    add folders & files

---
+++ hash.py
@@ -0,0 +1,11 @@
+import hashlib
+def calculate_sha256(file_path):
+    sha256 = hashlib.sha256()
+    with open(file_path, 'rb') as f:
+        while chunk := f.read(8192):
+            sha256.update(chunk)
+    return sha256.hexdigest()
+
+print(calculate_sha256('./inserted/H.md'))
+print(calculate_sha256('./inserted/T.md'))
+print(calculate_sha256('./inserted/P.md'))
